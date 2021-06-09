2e722d4611755adc709ce9dad43a9bc48964d2b9
Nabil Fawwaz Elqayyim <Nabil.FawwazElqayyim@Chubb.com>
2021-06-09 20:37:58 +0700

    Disable create tables on startup

--- user-service/src/main/java/com/nabil/udemy/ecommerce/userservice/service/DataSetupService.java
+++ user-service/src/main/java/com/nabil/udemy/ecommerce/userservice/service/DataSetupService.java
@@ -7,10 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.Resource;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
-
-import java.nio.charset.StandardCharsets;
 
 /**
  * @author Nabil Fawwaz Elqayyim
@@ -26,7 +22,7 @@ public class DataSetupService implements CommandLineRunner {
     private final R2dbcEntityTemplate entityTemplate;
 
     @Override // Create table on startup application
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 //        String query = StreamUtils.copyToString(initSql.getInputStream(), StandardCharsets.UTF_8);
 //        System.out.println(query);
 //        entityTemplate
